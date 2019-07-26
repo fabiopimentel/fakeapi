91db9be8eec17eb65d8a6be9f137f180c34dd338
Fábio <fabio.pimentel@olxbr.com>
2019-07-26 15:19:32 -0300

    Firts commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+var bodyParser = require("body-parser");
+
+const brandsJson = require("./brands.json");
+
+function randomInt(low, high) {
+    return Math.floor(Math.random() * (high - low) + low)
+}
+
+const app = express()
+app.use(bodyParser.json());
+const port = 3031
+const brandsBySecret = new Map()
+
+app.get('/brand', (req, res) => {
+
+    const secret = req.query.secret
+    if (!secret) {
+        res.json(brandsJson)
+    } else {
+        const brands = brandsBySecret.get(secret)
+        if (brands) {
+            res.send(brandsBySecret.get(secret))
+        } else {
+            res.send([])
+        }
+    }
+})
+
+app.post('/brand', function (req, res) {
+    let secret = req.body.secret;
+    let brands = brandsBySecret.get(secret)
+    if (brands) {
+        brands.push({name: req.body.name, id: brands.length + 1})
+    } else {
+        brands = [{name: req.body.name, id: 1}]
+        brandsBySecret.set(secret, brands)
+    }
+    res.send(brands)
+});
+
+
+app.listen(port, () => console.log(`Fake Fipe Api listening on port ${port}!`))
